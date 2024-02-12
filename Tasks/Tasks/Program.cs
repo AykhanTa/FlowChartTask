10e1ba5f888a3e981b2131b1227b09fe4bf2000e
AykhanTa <aykhanft@code.edu.az>
2024-02-13 00:36:12 +0400

    feat:flowchart tasks

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region Task1
+//int n = 7;
+//if (n % 2 == 0)
+//{
+//    Console.WriteLine("Eded cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("Eded tekdir");
+//}
+
+#endregion
+
+#region Task2
+//int n = 41;
+//if (n % 21 == 0)   //(n%3==0 && n%7==0)
+//{
+//    Console.WriteLine("Eded 3-e ve 7-e qaliqsiz bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Eded 3-e ve 7-e qaliqsiz bolunmur");
+//}
+
+#endregion
+
+#region Task3
+//int n, m;
+//n = 12;
+//m = 5;
+//if(n%2==0 && m % 2 == 0)
+//{
+//    Console.WriteLine(n+m);
+//}
+//else
+//{
+//    Console.WriteLine(n-m);
+//}
+#endregion
+
+
+#region Task4
+//int temp = 23;
+//if (temp == 15)
+//{
+//    Console.WriteLine("Havanin temperaturu 15 derecedir");
+//}
+//else
+//{
+//    if (temp > 15)
+//    {
+//        Console.WriteLine("Hava istidir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Hava soyuqdur");
+//    }
+//}
+#endregion
